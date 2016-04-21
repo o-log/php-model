17204abe696b51be630b83c07f7dad5ef78748c4
ologinov <olognv@gmail.com>
2016-04-21 19:51:28 +0300

    1

--- PHPModelDemo/Config.php
+++ PHPModelDemo/Config.php
@@ -10,16 +10,18 @@ class Config
     {
         $conf = [];
         //$conf = \App\CommonConfig::get(); // uncomment this to inherit shared configuration part, stored to repository
-        
+
+        /*
         $conf['memcache_servers'] = [
             'localhost:11211'
         ];
+        */
         
         $conf['cache_lifetime'] = 60;
         $conf['return_false_if_no_route'] = true; // for local php server
         $conf['db'] = array(
             self::DB_NAME_PHPMODELDEMO => array(
-                'host' => 'localhost',
+                'host' => '127.0.0.1',
                 'db_name' => 'phpmodel',
                 'user' => 'root',
                 'pass' => '1'
--- phpmodel.sql
+++ phpmodel.sql
@@ -1,9 +1,10 @@
 array(
 'create table demo_model(id int not null auto_increment primary key, title varchar(255) not null default "") default charset utf8',
 'create table modeldemonode (id int not null auto_increment primary key, created_at_ts int not null default 0) engine InnoDB default charset utf8 /* rand6474 */;',
-'alter table modeldemonode add column body text   default ""  /* rand9947 */;',
+'alter table modeldemonode add column body text   not null  /* rand9947 */;',
 'alter table modeldemonode add column title varchar(255)  not null   default ""  /* rand7236 */;',
 'create table demoterm (id int not null auto_increment primary key, created_at_ts int not null default 0) engine InnoDB default charset utf8 /* rand5893 */;',
+'create table demotermtonode  (id int not null auto_increment primary key, created_at_ts int not null default 0) engine InnoDB default charset utf8 /* rand5893 */;',
 'alter table demotermtonode add column term_id int  not null    /* rand28764 */;',
 'alter table demotermtonode add column node_id int  not null    /* rand464119 */;',
 'alter table demotermtonode add foreign key(term_id) references demoterm(id);',
