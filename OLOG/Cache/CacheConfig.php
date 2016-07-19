d981373f50f7698fa02eb42d4d232c7ad469bf8b
ologinov <olognv@gmail.com>
2016-07-19 12:16:39 +0300

    1

--- OLOG/Cache/CacheConfig.php
+++ OLOG/Cache/CacheConfig.php
@@ -7,7 +7,7 @@ class CacheConfig
     static protected $servers_obj_arr = [];
 
     static public function addServerSettingsObj(MemcacheServerSettings $server_settings_obj){
-        $servers_obj_arr[] = $server_settings_obj;
+        self::$servers_obj_arr[] = $server_settings_obj;
     }
 
     static public function getServersObjArr(){
